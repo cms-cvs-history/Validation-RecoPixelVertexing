7725fa34f3e21e0bd900de986de0fcb0f9f41920
Aaron Dominguez <sha1-5aa85c276f68fbe9c87c461aa7144bbc42f7c82a@cern.ch>
2006-09-21 20:40:13 +0000

    1st version of validation.  Now includes links to tracks

--- test/PixelVertexVal.sh
+++ test/PixelVertexVal.sh
@@ -5,6 +5,5 @@ eval $(scramv1 runtime -sh)
 echo "===========> Validating Pixel Primary Vertex Reco with 10-muon samples......."
 echo "===========> Validating reconstruction....."
 cmsRun val-reco.cfg
-setenv REFFILE "../data/val-reco.out"
-setenv CURFILE "val-reco.out"
-diff -iub ${CURFILE} ${REFFILE}
+cat val-reco.out
+
