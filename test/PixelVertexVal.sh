0ff9dd9383ac4536edda0f4bfd902fb9401ae1eb
Aaron Dominguez <sha1-5aa85c276f68fbe9c87c461aa7144bbc42f7c82a@cern.ch>
2006-09-21 20:57:10 +0000

    Now also test for readback of stored vertexes

--- test/PixelVertexVal.sh
+++ test/PixelVertexVal.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# remove old output root file if it exists
+/bin/rm -f pixel_vertexing.root
+
 eval $(scramv1 runtime -sh)
 
 echo "===========> Validating Pixel Primary Vertex Reco with 10-muon samples......."
@@ -7,3 +10,6 @@ echo "===========> Validating reconstruction....."
 cmsRun val-reco.cfg
 cat val-reco.out
 
+echo "===========> Validating I/O....."
+cmsRun val-io.cfg
+cat val-io.out
